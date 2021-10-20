605312a62e5443ce56f13a9d0fb48d74d939bca5
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-10-20 14:17:22 +0200

    Add files via upload

---
+++ doga.py
@@ -0,0 +1,15 @@
+print (' 1. feladat: ')
+
+x = int (input(' Írjon be egy számot: '))
+
+y = int (input(' Írjon be egy másik számot: '))
+
+print (' A két szám összege: ', x + y)
+
+print (' A két szám különbsége: ', x - y)
+
+print (' A két szám szorzata: ', x * y)
+
+print (' A két szám hányadosa: ', x / y)
+
+
