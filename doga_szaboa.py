97b7a770750a8974118045af35fbab0b57f51d73
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2021-11-10 14:35:28 +0100

    Add files via upload

---
+++ doga_szaboa.py
@@ -0,0 +1,40 @@
+print ("1.feladat:")
+
+print("Kopott kredenc, üres szenesvödör:")
+print("a feledés rozsdaként szöszmötöl.")
+print("Lyukas lavór csupán az emlékezés,")
+print("megkezdett kenyérben felejtett kés.")
+
+
+print("2.feladat:")
+
+print("Kérem adja meg a teljes nevét!")
+print("Kérem adja meg a születési idejét!")
+print("Kérem adja meg a születési helyét!")
+print("Kérem adja meg a lakcímét!")
+
+
+print("3.feladat:")
+
+x = 1
+y = 5
+
+print(x,y)
+print(y,x)
+
+
+print("4.feladat:")
+
+a = 4
+b = 8
+print("Területe:",a*b)
+print("Kerülete:",2*(a+b))
+
+
+print("5.feladat:")
+
+a = 3
+b = 6
+c = 9
+print("Területe:",a*b/2)
+print("Kerülete:",a+b+c)
