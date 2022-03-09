c5cbc0cf919809835140d2a01d23f7f6b643c36c
SzAlex04 <90601557+SzAlex04@users.noreply.github.com>
2022-03-09 08:36:27 +0100

    Add files via upload

---
+++ gyaksz.py
@@ -0,0 +1,13 @@
+for x in range (1,90):
+    print (x)
+
+for y in range(2,91):
+    print (y)
+
+negyzet = 0
+i = 1
+negyzet*i
+while (negyzet<1000):
+    print (negyzet)
+    negyzet = i*i
+    i+=1
